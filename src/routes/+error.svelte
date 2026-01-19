14809210d54e4467fbbfd905b0152db6ed6d42ae
austin <eliottroussille@gmail.com>
2026-01-19 20:27:32 +0100

    add custom 404 error page with image and message

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<section>
+	<div class="flex h-screen flex-col items-center justify-center">
+		<div class="flex flex-col items-center justify-center">
+			<img src="/404.webp" alt="404" class="w-1/2" />
+			<h1 class="text-4xl font-bold text-gray-100">Désolé, il n'y a rien ici</h1>
+			<p class="text-grey-900">La page que vous demandez n'existe pas.</p>
+			<a href="/" class="mt-4 rounded-md bg-blue-500 px-4 py-2 text-white">Revenir à l'accueil</a>
+		</div>
+	</div>
+</section>
