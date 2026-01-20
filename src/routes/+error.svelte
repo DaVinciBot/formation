77c9b1361d533af5feb1e5b3d4d16dc3f84e627d
austin <eliottroussille@gmail.com>
2026-01-21 00:22:04 +0100

    fix: update image and link resolution in custom 404 error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,13 +1,16 @@
 <script>
+	import { resolve } from '$app/paths';
 </script>
 
 <section>
 	<div class="flex h-screen flex-col items-center justify-center">
 		<div class="flex flex-col items-center justify-center">
-			<img src="/404.webp" alt="404" class="w-1/2" />
+			<img src={resolve('/') + '404.webp'} alt="404" class="w-1/2" />
 			<h1 class="text-4xl font-bold text-gray-100">Désolé, il n'y a rien ici</h1>
 			<p class="text-grey-900">La page que vous demandez n'existe pas.</p>
-			<a href="/" class="mt-4 rounded-md bg-blue-500 px-4 py-2 text-white">Revenir à l'accueil</a>
+			<a href={resolve('/')} class="mt-4 rounded-md bg-blue-500 px-4 py-2 text-white"
+				>Revenir à l'accueil</a
+			>
 		</div>
 	</div>
 </section>
