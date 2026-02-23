6817c0dba37029e14a338e934d3fd5da18e5653f
austin <eliottroussille@gmail.com>
2026-02-23 19:14:03 +0100

    update Supabase client configuration with new URL and key

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = `https://supa.davincibot.fr`;
-const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NjczNzAxOTcsImV4cCI6MTg5MzQ1NjAwMCwicm9sZSI6ImFub24iLCJpc3MiOiJzdXBhYmFzZSJ9.Wjq9SBB-CVUtItv1IzDBk7NKopA-OSd-VvNMQc6xpXU`;
+const supabaseUrl = `https://lxilsopqfrtwsitzalkm.supabase.co/`;
+const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4aWxzb3BxZnJ0d3NpdHphbGttIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk0NzUyNDUsImV4cCI6MTk4NTA1MTI0NX0.8TLXDbdJDIrDONJlqE639jvExH-kdBUIkJhtBFwhMCo`;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
