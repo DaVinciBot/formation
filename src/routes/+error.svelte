bc12d5f0f5e4fadf8df0b30c615873873740d317
austin <eliottroussille@gmail.com>
2026-06-09 22:06:01 +0200

    fix: update subproject commit reference and adjust image source in error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,11 +1,11 @@
-<script>
+<script lang="ts">
 	import { resolve } from '$app/paths';
 </script>
 
 <section>
 	<div class="flex min-h-[calc(100vh-5rem)] flex-col items-center justify-center">
 		<div class="flex flex-col items-center justify-center">
-			<img src={resolve('/') + '404.webp'} alt="404" class="w-1/2" />
+			<img src="/404.webp" alt="404" class="w-1/2" />
 			<h1 class="text-4xl font-bold text-gray-100">Désolé, il n'y a rien ici</h1>
 			<p class="text-grey-900">La page que vous demandez n'existe pas.</p>
 			<a href={resolve('/')} class="mt-4 rounded-md bg-blue-500 px-4 py-2 text-white"
